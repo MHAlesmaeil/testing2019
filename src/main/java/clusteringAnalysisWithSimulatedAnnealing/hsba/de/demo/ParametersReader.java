2cc04208573803d1dbe5485b42d8c92cd5b8b272
Mohamad Hamdo Alesmaeil <mohamad.h.alesmeil@gmail.com>
2019-05-20 09:08:38 +0200

    Comments have been updated. final check need to take place before submitting the written code.

--- src/main/java/clusteringAnalysisWithSimulatedAnnealing/hsba/de/demo/ParametersReader.java
+++ src/main/java/clusteringAnalysisWithSimulatedAnnealing/hsba/de/demo/ParametersReader.java
@@ -42,9 +42,9 @@ public class ParametersReader {
                System.out.println("To use this tool you need to have two files:");
                System.out.println("1. File which contains the points of dataset which needs to be in '.csv' format");
                System.out.println("2. File which contains the parameter which also needs to be in '.csv' format");
-                System.out.println("#################################");
-                System.out.println("## Assumptions and Expectation ##");
-                System.out.println("#################################");
+               System.out.println("#################################");
+               System.out.println("## Assumptions and Expectation ##");
+               System.out.println("#################################");
                System.out.println("The assumption for the first file is: The file is in '.csv' format, it " +
                        "does have a header, the values of the points are numerical and each point does have" +
                        " a unique key which is the first value in the point array");
@@ -52,19 +52,19 @@ public class ParametersReader {
                        " as it has the following pieces of information:{Please Note: Recommendations in this" +
                        " section are based on the developer personal experiences, you are however encouraged " +
                        "to choose you own values of the parameters}");
-               System.out.println("clusteringMethod,'2' {1: Single Linkage, 2: Simulated Annealing SMC," +
+               System.out.println("1- clusteringMethod,'2' {1: Single Linkage, 2: Simulated Annealing SMC," +
                        " 3: Simulated Annealing MMC, 4: All the previous methods}\n" +
-                       "numberOfClusters,'5'{No specific recommendation}\n" +
-                       "distanceMethods,'1' {1: Euclidean Distance, 2: Manhattan Distance }\n" +
-                       "numberOfIterationSMC,'10' {This parameter will be multiplied with the number of " +
+                       "2- numberOfClusters,'5'{No specific recommendation}\n" +
+                       "3- distanceMethods,'1' {1: Euclidean Distance, 2: Manhattan Distance }\n" +
+                       "4- numberOfIterationSMC,'10' {This parameter will be multiplied with the number of " +
                        "points in the dataset}\n" +
-                       "acceptanceTemperature,'0.5' {For a small dataset '0.5' is recommended}\n" +
-                       "initialTemperature,'2'{For a small dataset, '2.0' is recommended}\n" +
-                       "finalTemperature,'0.5' {For a small dataset, '0.5' is recommended}\n" +
-                       "coolingRate,'0.05' {No Specific recommendation}\n" +
-                       "mutationFactor,'0.01' {Between '0.05' and '0.01' is recommended}\n" +
-                       "showResultInConsole,'false' {For a large dataset, 'false' is recommended}\n" +
-                       "numberOfIterationPerGivenTemperature,'4' {This parameters will ensure to get a " +
+                       "5- acceptanceTemperature,'0.5' {For a small dataset '0.5' is recommended}\n" +
+                       "6- initialTemperature,'2'{For a small dataset, '2.0' is recommended}\n" +
+                       "7- finalTemperature,'0.5' {For a small dataset, '0.5' is recommended}\n" +
+                       "8- coolingRate,'0.05' {No Specific recommendation}\n" +
+                       "9- mutationFactor,'0.01' {Between '0.05' and '0.01' is recommended}\n" +
+                       "10- showResultInConsole,'false' {For a large dataset, 'false' is recommended}\n" +
+                       "11- numberOfIterationPerGivenTemperature,'4' {This parameters will ensure to get a " +
                        "certain number of iteration which is equal to number of points times this parameters}");
                System.out.println("The order in which those parameters are placed is critical to let the tool " +
                        "work as expected");
@@ -85,11 +85,11 @@ public class ParametersReader {
                 // Check if the path goes to a csv file
                 if (checkPath(filePath)){
                     // load the list of points
-                    listOfPoints= execute(filePath);
+                    listOfPoints= readListOfPoints(filePath);
                     System.out.println("File 'Points' has been found and accepted. Total number of points is: "+ (listOfPoints.size()-1));
                     if (checkPath(parameterPath)){
                         // load the parameters
-                       List<String[]> listOfParameters = execute(parameterPath);
+                       List<String[]> listOfParameters = readListOfPoints(parameterPath);
                         System.out.println("File 'Parameters' has been found and total number of read parameters is "+ (listOfParameters.size()-1));
                         executreOrderInParametersFile(listOfParameters,listOfPoints);
                     }else{
@@ -109,13 +109,13 @@ public class ParametersReader {
                 }
                 if (checkPath(inputString)){
                     System.out.println("Path is correct and has been accepted");
-                    listOfPoints= execute(inputString);
+                    listOfPoints= readListOfPoints(inputString);
                     if (listOfPoints.size()!=0){
                         System.out.println("File has been read and number of points is: "+ (listOfPoints.size()-1));
                         System.out.println("Please enter the path to the parameters file");
                         inputString = scanner.nextLine();
                         if (checkPath(inputString)){
-                            List<String[]> listOfParameters = execute(inputString);
+                            List<String[]> listOfParameters = readListOfPoints(inputString);
                             System.out.println("Parameters file has been read");
                             executreOrderInParametersFile(listOfParameters,listOfPoints);
 
@@ -134,8 +134,10 @@ public class ParametersReader {
         }
     }
 
-
-    public List<String[]> execute(String pathToDataFile)throws Exception {
+    /**
+     * Reading the list of the points from the file
+    */
+    public List<String[]> readListOfPoints(String pathToDataFile)throws Exception {
         Reader reader1 = Files.newBufferedReader(Paths.get(pathToDataFile));
         CSVReader csvReader = new CSVReader(reader1);
         List<String[]> listOfStrings = new ArrayList<>();
@@ -145,6 +147,10 @@ public class ParametersReader {
         return listOfStrings;
 
     }
+
+    /**
+     * Check if the given path is correct
+    */
     public boolean checkPath(String pathToDataFile)throws Exception{
 
         String SAMPLE_CSV_FILE_PATH = pathToDataFile;
@@ -162,12 +168,20 @@ public class ParametersReader {
             }
         }
     }
+
+    /**
+     * Print out the given parameters
+    */
     public void printOutParatmers(List<String []> listOfParametersToBePrintout){
         System.out.println("The Next Parameter were given and will be applied");
         for (int x= 1; x<listOfParametersToBePrintout.size();x++){
             System.out.println(x+"- "+listOfParametersToBePrintout.get(x)[0] +" is set to "+listOfParametersToBePrintout.get(x)[1] );
         }
     }
+
+    /**
+     * Execute the orders in the parameters
+    */
     public void executreOrderInParametersFile(List<String[]> listOfParameters, List<String[]> listOfPoints)throws Exception{
         printOutParatmers(listOfParameters);
         SimulatedAnnealingMMC simulatedAnnealingMMC = new SimulatedAnnealingMMC();
@@ -178,24 +192,24 @@ public class ParametersReader {
         int numberOfClusters = new Integer(listOfParameters.get(2)[1]);
         boolean showResultInConsole = new Boolean(listOfParameters.get(10)[1]);
         int numberOfIteration = new Integer(listOfParameters.get(4)[1])*listOfPoints.size();
-        double acceptanceTemperature = new Double(listOfParameters.get(5)[1]).doubleValue();
-        double initialTemperature = new Double(listOfParameters.get(6)[1]).doubleValue();
-        double mutationFactor = new Double(listOfParameters.get(9)[1]).doubleValue();
-        double finalTemperature = new Double(listOfParameters.get(7)[1]).doubleValue();
-        double alphaValue = new Double(listOfParameters.get(8)[1]).doubleValue();
+        double acceptanceTemperature = new Double(listOfParameters.get(5)[1]);
+        double initialTemperature = new Double(listOfParameters.get(6)[1]);
+        double mutationFactor = new Double(listOfParameters.get(9)[1]);
+        double finalTemperature = new Double(listOfParameters.get(7)[1]);
+        double alphaValue = new Double(listOfParameters.get(8)[1]);
         int numberOfIterationPerGivenTemperature = new Integer(listOfParameters.get(11)[1]);
         if (clusteringMethod==1){
-            // execute single linkage
+            // readListOfPoints single linkage
             System.out.println("Single Linkage is executing");
 
             singleLinkage.computeCluster(numberOfClusters,listOfPoints,distanceMehods,showResultInConsole);
         }else if (clusteringMethod ==2){
-            // execute simulated annealing SMC
+            // readListOfPoints simulated annealing SMC
             System.out.println("Simulated Annealing SMC is executing");
 
             simulatedAnnealingSMC.computeCluster(numberOfClusters,listOfPoints,numberOfIteration, acceptanceTemperature,mutationFactor,showResultInConsole);
         }else if (clusteringMethod ==3 ){
-            // execute simulated annealing MMC
+            // readListOfPoints simulated annealing MMC
             System.out.println("Simulated Annealing MMC is executing");
 
             simulatedAnnealingMMC.computeCluster(numberOfClusters,listOfPoints,initialTemperature,finalTemperature,alphaValue,mutationFactor,numberOfIterationPerGivenTemperature,showResultInConsole);
